13e95fa7aa53008629d3bee63dc5e68566bf7f45
Flechazo <2558755403@qq.com>
2026-05-12 13:51:25 +0800

    添加 close API #2

---
+++ src/main/java/cc/sighs/auratip/network/CloseTipPacket.java
@@ -0,0 +1,21 @@
+package cc.sighs.auratip.network;
+
+import cc.sighs.auratip.AuraTip;
+import cc.sighs.auratip.client.TipClient;
+import cc.sighs.oelib.network.api.INetworkContext;
+import cc.sighs.oelib.network.api.INetworkPacket;
+import cc.sighs.oelib.network.api.NetworkPacket;
+import cc.sighs.oelib.network.api.Side;
+
+@NetworkPacket(
+        modId = AuraTip.MOD_ID,
+        id = "close_tip",
+        side = Side.CLIENT
+)
+public record CloseTipPacket() implements INetworkPacket<CloseTipPacket> {
+
+    @Override
+    public void handle(INetworkContext context) {
+        context.enqueueWork(TipClient::closeCurrentTip);
+    }
+}
